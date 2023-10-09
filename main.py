fc84b7f38b78edad987a7497efbfa7ce6ebca21e
Blake Sanie <bsanie3@gatech.edu>
2023-10-09 16:13:57 -0400

    trying to solve torch issues

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-# %load_ext autoreload
-# %autoreload 2
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# get_ipython().run_line_magic('load_ext', 'autoreload')
+# get_ipython().run_line_magic('autoreload', '2')
 
 import sys
 import os
@@ -11,15 +17,38 @@ import torchvision.models as models
 from torchvision import transforms
 from datasets import load_dataset, concatenate_datasets
 
+
+# In[2]:
+
+
 resnet50 = models.resnet50(pretrained=True)
+
+
+# In[3]:
+
 
 from EarlyExitModel import EarlyExitModel
 
 model = EarlyExitModel(resnet50, 1000)
 model
 
+
+# In[4]:
+
+
 model.clear_exits()
 exit_layers = [model.add_exit(layer) for layer in ('layer1', 'layer2', 'layer3')]
+model
+
+
+# In[5]:
+
+
+# dataset = datasets.ImageFolder('./imagenet1k')
+
+
+# In[8]:
+
 
 hf_dataset = load_dataset("frgfm/imagenette", '320px')
 hf_dataset = concatenate_datasets(hf_dataset.values())
@@ -38,7 +67,7 @@ class CustomDataset(Dataset):
         if self.transform is not None:
             sample = self.transform(sample)
         label = record['label']
-
+        print(sample, label)
         return sample, label
 
 transform = transforms.Compose([
@@ -71,11 +100,45 @@ test_dataloader = DataLoader(
 )
 
 
+# In[10]:
+
+
 for layer in exit_layers:
-    layer.force_exit(True)
+    layer.force_forward()
 
 for X, y in train_dataloader:
     y_hat, exits = model(X)
-    print('exits', exits)
+    print('exits',exits)
     print(y_hat)
     break
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[12]:
+
+
+# get_ipython().system('{sys.executable} -m jupyter nbconvert --to script main.ipynb')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
